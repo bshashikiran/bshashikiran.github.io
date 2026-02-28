0ea8e2b92fa292a23ddf1b81eb10b1fe5ad8946f
bshashikiran <bolleddulashashikiran@gmail.com>
2026-02-28 19:29:33 +0530

    Update: GA | Event for Contact

--- src/constants/site.js
+++ src/constants/site.js
@@ -10,25 +10,25 @@ export const SITE = {
     social: [
         {
             name: "Email",
-            gaLabel: "contact_click",
+            gaLabel: "email_click",
             url: "mailto:bolleddulashashikiran@gmail.com",
             icon: MdEmail,
         },
         {
             name: "LinkedIn",
-            gaLabel: "contact_click",
+            gaLabel: "linkedin_click",
             url: "https://linkedin.com/in/bshashikiran",
             icon: FaLinkedin,
         },
         {
             name: "GitHub",
-            gaLabel: "contact_click",
+            gaLabel: "github_click",
             url: "https://github.com/bshashikiran",
             icon: FaGithub,
         },
         {
             name: "LeetCode",
-            gaLabel: "contact_click",
+            gaLabel: "leetcode_click",
             url: "https://leetcode.com/u/bshashikiran",
             icon: SiLeetcode,
         },
